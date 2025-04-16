b6f844ac24016700142496c4d92bf67963782ba5
chumpitized <126842502+chumpitized@users.noreply.github.com>
2025-04-16 14:21:24 -0500

    add a nice example in main, some comments, and use the new, aligned offset in commit_memory instead of curr_offset

--- src/main.cpp
+++ src/main.cpp
@@ -45,21 +45,14 @@ uptr align_forward(uptr ptr, size_t align) {
 	return ptr;
 }
 
-size_t commit_memory(Arena *a, size_t size) {
-	size_t total_alloc = a->curr_offset + size;
+size_t commit_memory(Arena *a, size_t offset, size_t size) {
+	size_t total_alloc = offset + size;
 
-	if (total_alloc <= a->committed) return -1;
+	if (total_alloc <= a->committed) return 0;
 
 	size_t diff = total_alloc - a->committed;
 
-	//use bitwise operators to avoid conditional branch...
 	return (diff + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1);
-
-	//if (diff % PAGE_SIZE == 0) {
-	//	return diff;
-	//} else {
-	//	return ((diff / PAGE_SIZE) + 1) * PAGE_SIZE;
-	//}
 }
 
 void *arena_alloc(Arena *a, size_t size, size_t align) {
@@ -70,9 +63,9 @@ void *arena_alloc(Arena *a, size_t size, size_t align) {
 	if (offset + size <= a->length) {
 		void *ptr = &a->buffer[offset];
 
-		size_t to_commit = commit_memory(a, size);
-		if (to_commit) {
-			VirtualAlloc(&a->buffer[a->curr_offset], to_commit, MEM_COMMIT, PAGE_READWRITE);
+		size_t to_commit = commit_memory(a, offset, size);
+		if (to_commit > 0) {
+			VirtualAlloc(&a->buffer[offset], to_commit, MEM_COMMIT, PAGE_READWRITE);
 			a->committed += to_commit;
 		}
 
@@ -89,6 +82,38 @@ void arena_clear(Arena *a) {
 }
 
 int main() {
-	std::cout << "hello" << std::endl;
+	size_t mb = 1024 * 1024;
+	Arena a = {0};
+	arena_init(&a, mb);
+
+	//This loop allocates 1024 ints in the arena.
+	//You might expect these allocations to consume only 4096 bytes (i.e., 1024 * sizeof(int)),
+	//but because we're aligning allocations on 8-byte boundaries (the size of a void pointer),
+	//each int allocation effectively consumes 8 bytes. The same thing would be true for chars, 
+	//which are only 1 byte wide.
+
+	//Not that the final allocation puts the curr_offset at 8188, just shy of 8192, which is
+	//exactly PAGE_SIZE * 2. I did that on purpose.
+	for (int i = 0; i < 1024; ++i) {
+		std::cout << "Allocating int..." << std::endl;
+		arena_alloc(&a, 4, DEFAULT_ALIGNMENT);
+		std::cout << "Current offset is " << a.curr_offset << std::endl;
+	}
+
+	//Because we're allocating 4096 bytes worth of ints and 4092 bytes worth of alignment padding (8188 bytes total),
+	//we will need to commit two pages of virtual memory. We confirm that here.
+	std::cout << std::endl;
+	std::cout << "Committed should be 8192: " << a.committed << std::endl;
+
+	//The very next int allocation should cause our arena to commit a third page of virtual memory.
+	std::cout << std::endl;
+	std::cout << "Allocating int..." << std::endl;
+	arena_alloc(&a, 4, DEFAULT_ALIGNMENT);
+	std::cout << "Current offset is " << a.curr_offset << std::endl;
+
+	//And finally we confirm the commitment of the third page of virtual memory. Cool!
+	std::cout << std::endl;
+	std::cout << "Committed should be 12288: " << a.committed << std::endl;
+
 	return 0;
 }
